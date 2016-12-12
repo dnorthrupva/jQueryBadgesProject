303b53a4fae73e4f4ef22c63ef6c5df99b95387a
Donavin Northrup <donavin@dnorthrupga.com>
2016-12-12 08:47:48 -0500

    Finish build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/TeoDaTank.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function(k,v){
+       console.log(k,v)
+       $('#badges').append('<div class="course"><h3>'+v.title+'</h3><img src="'+v.badge+'"><a href="'+v.url+'" class="btn btn-primary" target=_blank>See Course</a></div>')
+      })
+    }
+  });
 
 });
